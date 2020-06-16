a47b7e73596c6d9a4f74cff9f2b9c72aad06ece5
Zach Kellogg <Zachskellogg@gmail.com>
2020-06-15 20:13:57 -0600

    seeds and schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,9 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+  
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Zach Burger", true), ("Bacon Burger", true);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Black Bean Burger", true);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Wagu Bruger", false);
